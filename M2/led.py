d7efdd4afba1b93186f0c2b7da685e80011c67fe
waffe_x20 <argueta_eduardo@yahoo.de>
2023-04-29 22:59:22 -0600

    Add caesar and lead

--- M2/led.py
+++ M2/led.py
@@ -1,10 +1,17 @@
-patterns = ["###\n# #\n# #\n# #\n###", 
-            "#\n#\n#\n#\n#\n#", "###\n  #\n###\n#  \n###", "###\n  #\n###\n  #\n###",
-            "# #\n# #\n###\n  #\n  #", "###\n#  \n###\n  #\n###", "###\n#  \n###\n# #\n###",
-            "###\n  #\n  #\n  #\n  #", "###\n# #\n###\n# #\n###", "###\n# #\n###\n  #\n  #"]
+patterns = [["###","# #","# #","# #","###"], [" # "," # "," # "," # ", " # "],
+            ["###","  #","###","#  ","###"], ["###","  #","###","  #","###"],
+            ["# #", "# #","###", "  #", "  #"], ["###", "#  ", "###", "  #", "###"],
+            ["###", "#  ", "###", "# #", "###"], ["###", "  #",  "  #","  #","  #"], 
+            ["###", "# #", "###", "# #", "###"], ["###", "# #", "###", "  #",  "  #"]]
 
-def display_led(str):
-    for n in str:
-        print(patterns[int(n)])
+def display_led(numbers):
+    """
+    a program which is able to simulate the work of a seven-display device
+    """
+    for i in range(0, 5):
+        for number in numbers:
+            print(patterns[int(number)][i], end=" ")
+        print("\n")
 
+display_led("0123456789")
 display_led("123")
